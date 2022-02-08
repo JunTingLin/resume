9d3959c0b993be47bface51db7eaafe61da838fb
JunTing <10355098@st.thsh.tp.edu.tw>
2022-02-08 18:46:35 +0800

    change to php (about_me contact page)

---
+++ about_me.php
@@ -0,0 +1,123 @@
+<section data-id="about-me" class="animated-section">
+            <div class="page-title">
+              <h2>關於 <span>我</span></h2>
+            </div>
+
+            <div class="section-content">
+              <!-- Personal Information -->
+              <div class="row">
+                <div class="col-xs-12 col-sm-7">
+                  <p>父母都是資訊人，從小受父母薰陶，整天沉浸在coding的樂趣當中；<br>
+                    除此之外也喜愛攝影美工、剪輯影片，給畫面增添色彩，給影片增加趣味。<br>
+                    目前就讀中大資管系，朝著自己兩大夢想前進</p>
+                </div>
+
+                <div class="col-xs-12 col-sm-5">
+                  <div class="info-list">
+                    <ul>
+                      <li>
+                        <span class="title">年齡</span>
+                        <span class="value">19</span>
+                      </li>
+
+                      <li>
+                        <span class="title">現居</span>
+                        <span class="value">台北</span>
+                      </li>
+
+                      <li>
+                        <span class="title">e-mail</span>
+                        <span class="value">109403537@cc.ncu.edu.tw</span>
+                      </li>
+
+                      <li>
+                        <span class="title">電話</span>
+                        <span class="value">0966777000</span>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <!-- End of Personal Information -->
+
+              <div class="white-space-50"></div>
+
+              <!-- Services -->
+              <div class="row">
+                <div class="col-xs-12 col-sm-12">
+                  <div class="block-title">
+                    <h3>我的 <span>專業</span></h3>
+                  </div>
+                </div>
+              </div>
+
+              <div class="row">
+                <div class="col-xs-12 col-sm-6">
+                  <div class="col-inner">
+                    <div class="info-list-w-icon">
+                      <div class="info-block-w-icon">
+                        <div class="ci-icon">
+                          <i class="far fa-keyboard"></i>
+                        </div>
+                        <div class="ci-text">
+                          <h4>程式設計</h4>
+                          <p>我的母親是資訊管理學系畢業，我的父親是資訊工程學系畢業，在惠普科技相關企業做了20年，
+                            現在是一位專業資深經理人，所以想當然耳我從小就在資訊科學的環境中長大，
+                            對這塊巨大的領域也是非常著迷且竭盡所能去探索與挖掘，
+                            高中就拿下全校資訊科學競賽佳作與優等的好成績，甚至還拿下了全國高級中等學校閱讀心得寫作比賽優等(閱讀書目：大數據時代約無限商機)。
+                            目前持有兩張oracle Java國際證照。</p>
+                        </div>
+                      </div>
+                      <div class="info-block-w-icon">
+                        <div class="ci-icon">
+                          <i class="lnr lnr-laptop-phone"></i>
+                        </div>
+                        <div class="ci-text">
+                          <h4>網頁製作</h4>
+                          <p>雖然剛開始接觸網頁設計，但有程式跟美工基礎的我，讓我非常好上手，
+                            前端後端一條龍服務，我這位全端工程師通通都可以包辦</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="col-xs-12 col-sm-6">
+                  <div class="col-inner">
+                    <div class="info-list-w-icon">
+                      <div class="info-block-w-icon">
+                        <div class="ci-icon">
+                          <i class="fas fa-palette"></i>
+                        </div>
+                        <div class="ci-text">
+                          <h4>視覺設計</h4>
+                          <p>我的興趣也可說是資訊科技的一部份，我喜歡攝影、文書排版，
+                            從小就很有審美觀的我，在國小就拿下台北市兒童美術創作銅牌獎、美術比賽中年級繪畫佳作，
+                            部落格排版設計佳作，之前加入學校電繪社，自學PS、AI。
+                            高中學習檔案製作特優，甚至校慶還拿下攝影比賽第二名的殊榮，
+                            國三時是畢業紀念冊設計團員之一。
+                          </p>
+                        </div>
+                      </div>
+                      <div class="info-block-w-icon">
+                        <div class="ci-icon">
+                          <i class="fas fa-photo-video"></i>
+                        </div>
+                        <div class="ci-text">
+                          <h4>影片剪輯</h4>
+                          <p>平常會看一些剪輯大神們的影片(如:視頻大拍檔、影視颶風)，
+                            學會如何運鏡、用Adobe Premiere Pro剪輯、After Effects做特效，
+                            高三加入畢聯會，剪輯過班上畢業進場短片、謝師宴影片、該年度畢業歌MV。
+                            大學時也加入系學會新聞部，負責過系上密室逃脫宣傳影片剪輯、系上資管之夜影片剪輯。 </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <!-- End of Services -->
+
+              <div class="white-space-50"></div>
+
+            </div>
+          </section>
