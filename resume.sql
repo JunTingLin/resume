08a6b5833e9608ba5bf7d891ea32be3765890fa8
junting <10355098@st.thsh.tp.edu.tw>
2023-06-25 01:48:44 +0800

    update sql and data

--- resume.sql
+++ resume.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1:3307
--- 產生時間： 2023-06-23 13:13:13
+-- 產生時間： 2023-06-24 19:47:30
 -- 伺服器版本： 10.4.24-MariaDB
 -- PHP 版本： 8.1.6
 
@@ -45,12 +45,12 @@ CREATE TABLE `blogs` (
 --
 
 INSERT INTO `blogs` (`id`, `title`, `category`, `intro`, `people`, `tag`, `cover`, `pic`, `date_start`, `date_end`) VALUES
-(1, '迎新宿營', '活動', '迎新宿營算是人生中蠻重要的活動，看似不起眼的兩天一夜露營其實收穫很大，因為我個性比較文靜， 但宿營的學長姐們就是會逼每個都要玩都要參與，也因為這樣我打開偶包放膽去玩，有的是動頭腦的有的是動態遊戲的， 真的玩得不亦樂乎，也因此交到大學許多知己，這個活動可以說是開啟大學生活的閃耀大門。', '宿營-第一小隊', '露營、大學之路、遊戲、朋友', 'img/blog/Camping1.JPG', 'img/blog/Camping2.JPG', '2020-09-26', '2020-09-27'),
-(2, '資管嘉年華-美宣組', '活動', '中央大學資管系資管嘉年華是大四學長姊們展現辛苦專題製作成果的活動，當時身為大一的我就主動加入活動策畫的一份子，我加入的是美宣組，主要負責海報製作、工作人員的名牌設計、拍貼機樣板圖片設計、場邊佈置、紀念品設計等等，並且在活動當天協助動線流程、拍照與攝影，光這個工作量可見要花非常多時間開會討論以及製作設計，還記得當初除了各組固定開會外，不同組(活動組、策展組)還要開三次籌會，兩次小籌，最後一次總籌。這個活動也讓我學習到Adobe Photoshop、Adobe Illustrator專業軟體、海報送印流程、攝影技巧、團隊合作等等，最後所有組員也去吃慶功宴慶祝這次活動的圓滿結束。', '美宣組組員', 'Photoshop、Illustrator、攝影、團隊合作、海報印刷', 'img/blog/MIS_carnival1.jpg', 'img/blog/MIS_carnival2.JPG', '2020-10-01', '2020-12-05'),
-(3, '系學會-新聞部', '活動', '當初面試上系學會新聞部時非常興奮，因為新聞部負責的系上內容剛好是我的最愛， 我喜愛剪輯攝影，在新聞部跟同儕切磋後，更增進了我這方面的知識，在部內除了負責 系上活動的攝影外，還要剪輯各式系上活動宣傳片(運動會、資管之夜、密室逃脫、傳情、送舊)， 也在學校舉辦過攝影比賽，除了技術上的知識外，更重要的是學習團隊合作與溝通。', '109新聞部部員', '系學會、新聞部、團隊合作、Adobe Premiere Pro 攝影、部出遊', 'img/blog/MIS_club1.jpg', 'img/blog/MIS_club2.jpg', '2020-10-01', '2021-09-01'),
-(4, '中央程式設計研究社', '活動', '加入程研社是我覺得收穫最多的，每次的社課都會請厲害的大神，舉凡學長姐或校友回來， 每次固定一個主題，印象較深的是linux，透過VM ware灌Ubuntu ISO，介紹常用指令，並透過 SSH建立本基端與虛擬主機的連線；還有像是git，我就是從上完那堂社課後才發現git的強大， 以前專案都複製好多份出來，命名copy1、copy2，既容易搞混又佔記憶體空間，有了這個強大的版本 控制系統，可以輕易管控版本，每個版本加上commit提交，更是便於多人撰寫專案，push上去pull回來 ，輕鬆搞定；當然還上了很多不同主題的社課，舉凡Laravel網頁框架、網頁安全CTF等等，都讓我獲益良多。', '程研社社員', 'Linux、Ubuntu、git、Laravel', 'img/blog/ncu_coding_club1.png', 'img/blog/ncu_coding_club2.png', '2021-02-02', NULL),
-(5, '109學年度-榮獲管理學院-院長獎', '頒獎', '能拿到管理學院院長獎，並由院長親自頒發感到非常開心， 我的態度一直是秉持著做每件事都要努力盡力，大學的時光內除了課業外，也參加社團(程式設計研究社)、 參加系學會(新聞部)，跑過學校淨灘、107電影院...等等休閒活動，另外也不斷自我摸索 上網學習探索自己，展望未來。', '管院獲獎學生', '努力、堅持、學習、獲獎、成就', 'img/blog/award1.jpg', 'img/blog/award2.jpg', '2021-12-28', '0000-00-00'),
-(6, 'Google Developer Student Club @ NCU', '活動', '除了參加程式設計研究社外， 這學期也開始加入學校Google Developer Student Club中央大學Google開發者社群的社課活動， 每隔幾周都會有不同的主題，大家一起討論一起實作，最有印象的是phpの黑魔法合輯：Laravel的活動， 了解到現在業界最常使用的框架MVC的技術， module、view、controller用controller來調用相關的view頁面，可以減輕工程師的負擔， 後續維護上也比較容易，不需要每一頁每一頁都修改相同的東西。 之後還接觸到google開放原始碼軟體套件Flutter， 透過線上FlutLab設計一個可以抓取中央氣象局api的UI介面， 學Flutter的好處也在於用一種程式語言(dart)寫一份Code能同時讓iOS與Android平台使用， 也支援Web以及桌面應用程式，一舉兩得。當然參加google club學到的還有Docker、goolge GCP等等。', 'Google Club社員', 'GCP、Flutter、Vertex AI、Laravel、Docker', 'img/blog/ncu_gdsc1.png', 'img/blog/ncu_gdsc2.png', '2022-05-16', NULL),
+(1, '迎新宿營', '活動', '迎新宿營算是人生中蠻重要的活動，看似不起眼的兩天一夜露營其實收穫很大，因為我個性比較文靜，但宿營的學長姐們就是會逼每個都要玩都要參與，也因為這樣我打開偶包放膽去玩，有的是動頭腦的有的是動態遊戲的，真的玩得不亦樂乎，也因此交到大學許多知己，這個活動可以說是開啟大學生活的閃耀之門。', '宿營-第一小隊', '露營、大學之路、遊戲、朋友', 'img/blog/Camping1.JPG', 'img/blog/Camping2.JPG', '2020-09-26', '2020-09-27'),
+(2, '資管嘉年華-美宣組', '活動', '中央大學資管系資管嘉年華是大四學長姊們展現辛苦專題製作成果的活動，當時身為大一的我就主動加入活動策畫的一份子，我加入的是美宣組，主要負責海報製作、工作人員的名牌設計、拍貼機樣板圖片設計、場邊佈置、紀念品設計等等，並且在活動當天協助動線流程、拍照與攝影，光這個工作量可見要花非常多時間開會討論以及製作設計，還記得當初除了各組固定開會外，不同組(活動組、策展組)還要開三次籌會，兩次小籌，最後一次總籌。這個活動也讓我學習到Adobe Photoshop、Adobe Illustrator專業軟體、海報送印流程、攝影技巧、團隊合作等等，最後所有組員也去吃慶功宴，慶祝這次活動的圓滿結束。', '美宣組組員', 'Photoshop、Illustrator、攝影、團隊合作、海報印刷', 'img/blog/MIS_carnival1.jpg', 'img/blog/MIS_carnival2.JPG', '2020-10-01', '2020-12-05'),
+(3, '系學會-新聞部', '活動', '當初面試上系學會新聞部時非常興奮，因為新聞部負責的系上內容剛好是我的最愛，我喜愛剪輯攝影，在新聞部跟同儕切磋後，更增進了我這方面的知識，在部內除了負責系上活動的攝影外，還要剪輯各式系上活動宣傳片(運動會、資管之夜、密室逃脫、傳情、送舊)，也在學校舉辦過攝影比賽，除了技術上的知識外，更重要的是學習團隊合作與溝通。', '109新聞部部員', '系學會、新聞部、團隊合作、Adobe Premiere Pro 攝影、部出遊', 'img/blog/MIS_club1.jpg', 'img/blog/MIS_club2.jpg', '2020-10-01', '2021-09-01'),
+(4, '中央程式設計研究社', '活動', '加入程研社是我覺得收穫最多的，每次的社課都會請厲害的大神，舉凡學長姐或校友回來，每次固定一個主題，印象較深的是Linux，透過VM ware灌Ubuntu ISO，介紹常用指令，並透過SSH建立本機端與虛擬主機的連線；還有像是git，我就是從上完那堂社課後才發現git的強大，以前專案都複製好多份出來，命名copy1、copy2，既容易搞混又佔記憶體空間，有了這個強大的版本控制系統，可以輕易管控版本，每個版本加上commit提交，更是便於多人撰寫專案，push上去pull回來，輕鬆搞定；當然還上了很多不同主題的社課，舉凡Laravel網頁框架、網頁安全CTF等等，都讓我獲益良多。', '程研社社員', 'Linux、Ubuntu、git、Laravel', 'img/blog/ncu_coding_club1.png', 'img/blog/ncu_coding_club2.png', '2021-02-02', NULL),
+(5, '109學年度-榮獲管理學院-院長獎', '頒獎', '能拿到管理學院院長獎，並由院長親自頒發感到非常開心，我的態度一直是秉持著做每件事都要努力盡力，大學的時光內除了課業外，也參加社團(程式設計研究社)、參加系學會(新聞部)，跑過學校淨灘、107電影院...等等休閒活動，另外也不斷自我摸索，上網學習探索自己，展望未來。', '管院獲獎學生', '努力、堅持、學習、獲獎、成就', 'img/blog/award1.jpg', 'img/blog/award2.jpg', '2021-12-28', '0000-00-00'),
+(6, 'Google Developer Student Club @ NCU', '活動', '除了參加程式設計研究社外，這學期也開始加入學校Google Developer Student Club中央大學Google開發者社群的社課活動，每隔幾週都會有不同的主題，大家一起討論一起實作，最有印象的是phpの黑魔法合輯：Laravel的活動，了解到現在業界最常使用的框架MVC的技術， Module、View、Controller用Controller來調用相關的view頁面，可以減輕工程師的負擔，後續維護上也比較容易，不需要每一頁每一頁都修改相同的東西。之後還接觸到Google開放原始碼軟體套件Flutter， 透過線上FlutLab設計一個可以抓取中央氣象局Api的UI介面，學Flutter的好處也在於用一種程式語言(Dart)寫一份Code能同時讓ios與Android平台使用， 也支援Web以及桌面應用程式，一舉兩得。當然參加google club學到的還有Docker、goolge GCP等等。', 'Google Club社員', 'GCP、Flutter、Vertex AI、Laravel、Docker', 'img/blog/ncu_gdsc1.png', 'img/blog/ncu_gdsc2.png', '2022-05-16', NULL),
 (13, '110學年度-榮獲管理學院-院長獎', '頒獎', '很榮幸能再次獲得院長獎的殊榮，自己在大二上下兩學期付出的努力是有收穫的，也希望大三能夠繼續加油。', '管院獲獎學生', '努力 堅持 學習 獲獎 成就', 'img/blog/award3.jpg', 'img/blog/award4.jpg', '2022-12-27', '0000-00-00');
 
 -- --------------------------------------------------------
@@ -103,9 +103,9 @@ CREATE TABLE `experiences` (
 
 INSERT INTO `experiences` (`id`, `category`, `title`, `subtitle`, `intro`, `date_start`, `date_end`) VALUES
 (1, '教育', '中央大學', '資訊管理學系', '「資手遮天，管遍天下，中央資管，全國第一」，是中央資管系的口號， 除了宿營、系烤、系卡、資管營外，學校校運會也是中央資管系一年一度的一大盛事， 中央資管系在校運會的表現一直還不錯，但其實是因為長期準備練習使然， 「要這麼得？先學會怎麼栽！」，中央資管不只傳授專業科目，更啟發我們人生哲學， 努力不懈的精神。', '2020-09-01', '2024-07-01'),
-(2, '教育', '再興中學', '資優班', '從國中一路到高中，對再興有著濃厚的情感， 再興像是嚴父也像是慈母，管得非常嚴，有髮禁， 服裝統一整齊劃一，有晚自習周六也必須上課， 但本質都是為我們好，希望未來能有一番成就。', '2017-09-01', '2020-07-01'),
-(3, '工作', '家教', '電腦影片剪輯', '輔導學生使用剪輯軟體-威力導演完成影片專案，自編教材，從0到1，帶學生入手', '2020-07-01', '2020-09-01'),
-(4, '工作', '中央大學應地所', '網管', '大三開始很榮幸面試上中央大學應用地質研究所的網管工作，主要負責所上官網的維護，使用WordPress進行網站架設，定期備份與更新資料；另外會負責維護網路設備，DHCP、DNS、Web server，這些都是架在NAS主機上，並且配有UPS，要定期去掃ip、踢mac等工作；還有協助所上整個的資安上的安全防護。', '2022-09-12', NULL);
+(2, '教育', '再興中學', '資優班', '從國中一路到高中，對再興有著濃厚的情感， 再興之於我像是嚴父也像是慈母，管得非常嚴，有髮禁， 服裝統一整齊劃一，有晚自習周六也必須上課， 但本質都是為我們好，希望未來能有一番成就。', '2017-09-01', '2020-07-01'),
+(3, '工作', '中央大學應地所', '網管', '大三開始很榮幸面試上中央大學應用地質研究所的網管工作，主要負責所上官網的維護，使用WordPress進行網站架設，定期備份與更新資料；另外會負責維護網路設備，DHCP、DNS、Web server，這些都是架在NAS主機上，並且配有UPS，要定期去掃ip、踢mac等工作；還有協助所上整個的資安上的安全防護。', '2022-09-12', '0000-00-00'),
+(4, '工作', '家教', '電腦影片剪輯', '輔導學生使用剪輯軟體-威力導演完成影片專案，自編教材，從無到有，帶學生入手。', '2020-07-01', '2020-09-01');
 
 -- --------------------------------------------------------
 
@@ -223,10 +223,10 @@ INSERT INTO `works` (`id`, `title`, `category`, `intro`, `team`, `teacher`, `web
 (6, '蝦皮爬蟲&賣家競品分析(Python)', '專案', '在剛成為賣家不熟悉市場時 , 定價策略顯得尤其重要 為了找到在蝦皮上最佳銷售策略，所以撰寫了這支蝦皮賣家競品分析。 此次的目標很簡單，掌握競品情報來提高自己銷售業績：<br>\n1.找出商品的人氣特性<br>\n2.透過觀察價格分布決定價格,並預估收益<br>\n3.實際搜尋<br>\n4.找出賣家地理位置分布,決定賣場要設哪', '林俊霆、許珀維', '何政勳', 'https://github.com/brankhsu/python-project-crawler', 'Python、PyCharm', 'img/portfolio/project_python-crawler.jpg', 'img/portfolio/project_python-crawler_1.JPG', 'img/portfolio/project_python-crawler_2.JPG', 'img/portfolio/project_python-crawler_3.JPG', 'img/portfolio/project_python-crawler_4.JPG', '2021-12-29'),
 (7, '陳仲儼教授-教學傑出選拔', 'YouTube影片', '', '', '', 'https://www.youtube.com/embed/JIUy_HhOj-Q', '', 'img/portfolio/Professor_Chen1.png', '', '', '', '', '2022-02-27'),
 (8, '陳仲儼教授-教學傑出選拔v2', 'YouTube影片', '', '', '', 'https://www.youtube.com/embed/bbWvaSsORMQ', '', 'img/portfolio/Professor_Chen_v2.jpg', '', '', '', '', '2023-06-22'),
-(9, 'Multithread Web Socket Project', '專案', '本次OS作業要繳交Thread 小程式，因為對java比較熟悉，故選用java作為程式語言，用eclipse IDE開發。網路上看到許多用java多執行緒實現TCP網路的socket程式設計，但有鑑於求新求變，故基於TCP的架構下設計成可以「多人」連上sever「對玩」的井字小遊戲。', '林俊霆', '周立德', 'https://github.com/JunTingLin/multithread_project', 'java、eclipse、multi thread、web socket、java swing', 'img/portfolio/project_multithread.jpg', 'img/portfolio/project_multithread_1.png', 'img/portfolio/project_multithread_2.png', 'img/portfolio/project_multithread_3.png', 'img/portfolio/project_multithread_4.png', '2022-05-18'),
+(9, 'Multithread Web Socket Project', '專案', '本次OS作業要繳交Thread 小程式，因為對java比較熟悉，故選用java作為程式語言，用eclipse IDE開發。網路上看到許多用java多執行緒實現TCP網路的socket程式設計，但有鑑於求新求變，故基於TCP的架構下設計成可以「多人」連上server「對玩」的井字小遊戲。', '林俊霆', '周立德', 'https://github.com/JunTingLin/multithread_project', 'java、eclipse、multi thread、web socket、java swing', 'img/portfolio/project_multithread.jpg', 'img/portfolio/project_multithread_1.png', 'img/portfolio/project_multithread_2.png', 'img/portfolio/project_multithread_3.png', 'img/portfolio/project_multithread_4.png', '2022-05-18'),
 (10, '電影票房預測系統', '專案', '這份專案是我修習機器學習的成果，分成兩個部分。期中是個人書面報告，我選用的是Kaggle上的「票房收入預測」主題，在報告內敘述參賽的動機、選題原因、如何實作、跟上課的關聯性，最後推測還有哪方面可以改善，可以讓排名進步。期末的專題剛好也是類似主題，而我在期末團隊中扮演的角色是負責前端UI跟服務架站，讓世界各地的人都能使用我們的電影票房預測系統。', '期中: 林俊霆；期末: 吳昇亮、林俊霆、羅向榮、呂采璇、梁恩維', '鐘祥仁', 'https://github.com/JunTingLin/ML_Box-Office', 'Python、ML、Data Preprocessing、Kaggle', 'img/portfolio/project_ML-movie.jpg', 'img/portfolio/project_ML-movie.jpg', 'img/portfolio/project_ML-movie_2.jpg', 'img/portfolio/project_ML-movie_3.png', 'img/portfolio/project_ML-movie_4.png', '2023-06-23'),
-(11, 'NCU BookShop', '專案', '系統分析(SA)期末專案成品，此系統提供一個平台讓中央學生購買各教授的上課用書，系統上的書籍分類係根據系別來分類(例如：資工、資管、財金等)；會員可透過此系統瀏覽圖書和輸入關鍵字來搜尋需要的書籍，亦可點擊有興趣的書籍查看書籍詳細內容，如確定購買，可將訂購商品放入購物車，在購物車中可修改、刪除訂購數量；提供會員相關服務，如；圖書結帳、檢視訂單內容等。另提供後台管理員簡潔易上手的管理介面，使管理員從事管理作業(會員、書籍、訂單)更有效率。', '林俊霆、許珀維、王柏勛、賴思妤、陳佳妤、蘇湘婷', '葉羅堯', 'https://github.com/JunTingLin/BookShop-NCU-SA', 'java web、jsp、c3p0、commons-dbutils、jstl、commons-beanutils、commons-fileupload、Spring-security-crypto、mysql、springframework', 'img/portfolio/project_SA.jpg', 'img/portfolio/project_SA_1.png', 'img/portfolio/project_SA_2.png', 'img/portfolio/project_SA_3.png', 'img/portfolio/project_SA_4.png', '2023-06-22'),
-(12, 'Classroom Reservation System', '專案', '這個專案是為中央大學應用地質研究所開發的教室預約系統。考慮到以前使用 Google 表單和日曆進行預約的方式混亂且難以管理，且缺乏身份驗證，任何人都可以進行預約。因此，本次專案使用 Spring Boot 和 Nas 上的 MariaDB 資料庫開發了一個簡單的預約系統。系統採用學校計中帳號的 Portal 單一登入認證機制，僅允許在學學生進行單一預約，而批量預約（例如課表）僅限指定的管理員帳號使用。', '林俊霆、王柏勛', '無', 'https://github.com/JunTingLin/Classroom-reservation-system', 'java、spring boot、 MariaDB、NAS', 'img/portfolio/project_Classroom-reservation-system.jpg', 'img/portfolio/project_Classroom-reservation-system_1.png', 'img/portfolio/project_Classroom-reservation-system_2.png', 'img/portfolio/project_Classroom-reservation-system_3.png', 'img/portfolio/project_Classroom-reservation-system_4.png', '2023-06-22');
+(11, 'NCU BookShop', '專案', '系統分析(SA)期末專案成品，此系統提供一個平台讓中央學生購買各教授的上課用書，系統上的書籍分類係根據系別來分類(例如：資工、資管、財金等)；會員可透過此系統瀏覽圖書和輸入關鍵字來搜尋需要的書籍，亦可點擊有興趣的書籍查看書籍詳細內容，如確定購買，可將訂購商品放入購物車，在購物車中可修改、刪除訂購數量；提供會員相關服務，如；圖書結帳、檢視訂單內容等。另提供後台管理員簡單易上手的管理介面，使管理員從事管理作業(會員、書籍、訂單)更有效率。', '林俊霆、許珀維、王柏勛、賴思妤、陳佳妤、蘇湘婷', '葉羅堯', 'https://github.com/JunTingLin/BookShop-NCU-SA', 'java web、jsp、c3p0、commons-dbutils、jstl、commons-beanutils、commons-fileupload、Spring-security-crypto、mysql、springframework', 'img/portfolio/project_SA.jpg', 'img/portfolio/project_SA_1.png', 'img/portfolio/project_SA_2.png', 'img/portfolio/project_SA_3.png', 'img/portfolio/project_SA_4.png', '2023-06-22'),
+(12, 'Classroom Reservation System', '專案', '這個專案是為中央大學應用地質研究所開發的教室預約系統。考慮到以前使用 Google 表單和日曆進行預約的方式有點混亂難以管理，且缺乏身份驗證，任何人都可以進行預約。因此，本次專案使用 Spring Boot 和 Nas 上的 MariaDB 資料庫開發了一個簡單的預約系統。系統採用學校計中帳號的 Portal 單一登入認證機制，僅允許在學學生進行單一預約，而批量預約（例如課表）僅限指定的管理員帳號使用。', '林俊霆、王柏勛', '無', 'https://github.com/JunTingLin/Classroom-reservation-system', 'java、spring boot、 MariaDB、NAS', 'img/portfolio/project_Classroom-reservation-system.jpg', 'img/portfolio/project_Classroom-reservation-system_1.png', 'img/portfolio/project_Classroom-reservation-system_2.png', 'img/portfolio/project_Classroom-reservation-system_3.png', 'img/portfolio/project_Classroom-reservation-system_4.png', '2023-06-22');
 
 --
 -- 已傾印資料表的索引
@@ -294,7 +294,7 @@ ALTER TABLE `certificates`
 -- 使用資料表自動遞增(AUTO_INCREMENT) `experiences`
 --
 ALTER TABLE `experiences`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `knowledges`
